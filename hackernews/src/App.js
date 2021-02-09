0a3ac5883cc0adbf421abaeadff7ddef7b1ca894
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-02-09 12:55:50 -0600

    Pagina 30 Clases en React

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,31 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
 // Serian datos de una api
 const list = [
-    {
-      title: 'React',
-      url: 'https://facebook.github.io/react/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://github.com/reactjs/redux',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-function App() {
-  return (
-    <div className="App">
-      {list.map(item => {
-        return (
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -34,10 +41,10 @@ function App() {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
           </div>
-        )
-      })}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
