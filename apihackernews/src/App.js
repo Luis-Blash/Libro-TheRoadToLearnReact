60293db5122dcad6bcd01841807c6b64c7899158
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-02-10 16:52:33 -0600

    Pagina 86 Fetching data En esta parte agregamos la api de hackernews la cual quitamos algunos datos como list. Usando Fetch hacemos una peticion asincrona de nuestros datos para asi crear la lista, el boton de onDismiss ya no sirve porque no se actualiza la lista.

--- apihackernews/src/App.js
+++ apihackernews/src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+// Informacion para base
+const DEFAULT_QUERY = "a";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+    };
+    // Api
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then((response) => response.json())
+      .then((result) => this.setSearchTopStories(result))
+      .catch((error) => error);
+  }
+
+  // olvidar un dato de la lista
+  onDismiss(id) {
+    const updatedList = this.state.list.filter((item) => item.objectID !== id);
+    console.log(updatedList);
+    this.setState({ list: updatedList });
+  }
+
+  // Para el buscar
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    const { searchTerm, result } = this.state;
+    if (!result) {
+      return null;
+    }
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
+        <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
+      </div>
+    );
+  }
+}
+
+// Componente de busqueda
+function Search({ value, onChange, children }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      {children} <input type="text" value={value} onChange={onChange} />
+    </form>
   );
+}
+
+// Compoentes Table
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div className="table">
+        {list.filter(isSearched(pattern)).map((item) => (
+          <div key={item.objectID} className="table-row">
+            <span style={{ width: "40%" }}>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span style={{ width: "30%" }}>{item.author}</span>
+            <span style={{ width: "10%" }}>{item.num_comments}</span>
+            <span style={{ width: "10%" }}>{item.points}</span>
+            <span style={{ width: "10%" }}>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+                className="button-inline"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
