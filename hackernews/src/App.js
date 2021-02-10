5f770989bdb24f88e9fe71d256768c067648e89e
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-02-09 20:42:33 -0600

    Pagina 66 Componentes compostables Hay una pequeña propiedad más a la que se puede acceder en el objeto de utilería: el accesorio de niños.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -66,7 +66,9 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Search value={searchTerm} onChange={this.onSearchChange}>
+          Search
+        </Search>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
@@ -75,9 +77,10 @@ class App extends Component {
 
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;
+    const { value, onChange, children } = this.props;
     return (
       <form>
+        {children}
         <input type="text" value={value} onChange={onChange} />
       </form>
     );
